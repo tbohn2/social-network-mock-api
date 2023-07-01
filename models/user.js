479b318f7167d1dfd9cde649c94027f0224187fd
tbohn2 <tanner.bohn@yahoo.com>
2023-07-01 16:19:00 -0700

    Fixed keys; created virtual for counting friends

--- models/user.js
+++ models/user.js
@@ -9,17 +9,27 @@ const userSchema = new Schema(
             required: true,
             unique: true,
             trim: true
-
         },
         email: {
             type: String,
             required: true,
             unique: true,
+            match: /^\S+@\S+\.\S+$/,
         },
-        thoughts: [thoughtSchema],
-        friends: [userSchema]
+        thoughts: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Thought',
+        },],
+        friends: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+        },]
     },
 );
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
 
 const User = model('user', userSchema);
 
