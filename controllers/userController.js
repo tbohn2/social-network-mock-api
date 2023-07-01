7618ea823d393b9f278e41c3fcf0eec19561a88c
tbohn2 <tanner.bohn@yahoo.com>
2023-07-01 16:55:49 -0700

    Added the rest of CRUD for user

--- controllers/userController.js
+++ controllers/userController.js
@@ -28,4 +28,43 @@ module.exports = {
             return res.status(500).json(err);
         }
     },
+    async createUser(req, res) {
+        try {
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async updateUser(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params._id },
+                { $set: req.body },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                res.status(404).json({ message: 'User not found' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteUser(req, res) {
+        try {
+            const user = await User.findOneAndRemove({ _id: req.params._id });
+
+            if (!user) {
+                return res.status(404).json({ message: 'User does not exist' });
+            }
+
+            res.json({ message: 'Student successfully deleted' });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json(err);
+        }
+    },
 }
