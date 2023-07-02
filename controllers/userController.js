d3c017e5db743263a2892155e4326c4c777065c4
tbohn2 <tanner.bohn@yahoo.com>
2023-07-01 17:15:05 -0700

    Added routes to add and delete friends

--- controllers/userController.js
+++ controllers/userController.js
@@ -61,10 +61,45 @@ module.exports = {
                 return res.status(404).json({ message: 'User does not exist' });
             }
 
-            res.json({ message: 'Student successfully deleted' });
+            res.json({ message: 'User successfully deleted' });
         } catch (err) {
             console.log(err);
             res.status(500).json(err);
         }
     },
+    async addNewFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.params.friendId } },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    async deleteFriend(req, res) {
+        try {
+            const user = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: { _id: req.params.friendId } } },
+                { runValidators: true, new: true }
+            );
+
+            if (!user) {
+                return res.status(404).json({ message: 'No user found with that ID' });
+            }
+
+            res.json(user);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
 }
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -5,9 +5,13 @@ const {
     getOneUser,
     createUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    addNewFriend,
+    deleteFriend
 } = require('../../controllers/userController')
 
 router.route('/').get(getUsers).post(createUser)
 
 router.route('/:_id').get(getOneUser).put(updateUser).delete(deleteUser)
+
+router.route('/:userId/friends/:friendId').put(addNewFriend).delete(deleteFriend)
