830ec9667246044c5b5dbed2ee47704da6ae0c5f
tbohn2 <tanner.bohn@yahoo.com>
2023-07-01 17:00:33 -0700

    Added CRUD routes for user

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,2 +1,13 @@
 const router = require('express').Router();
 
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/userController')
+
+router.route('/').get(getUsers).post(createUser)
+
+router.route('/:_id').get(getOneUser).put(updateUser).delete(deleteUser)
