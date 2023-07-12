3ccf4f74b0e1738698ec404334769d2b5ba4c17a
tbohn2 <tanner.bohn@yahoo.com>
2023-07-12 10:56:59 -0700

    Added comments to connection.js and index.js

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 const { connect, connection } = require('mongoose');
 
+// Creates connection string for mongo database
 const connectionString =
   process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB';
 
+// Mongoose function to connect to database
 connect(connectionString);
 
 module.exports = connection;
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// Imports database connection from config
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
+// Once database is connected, the server listens for any requests
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
